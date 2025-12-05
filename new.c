020095f36f9d52806cf01d4997f6c92d37eba401
DecioLuvier <142108217+DecioLuvier@users.noreply.github.com>
2025-12-05 16:38:11 -0300

    Create new.c

---
+++ new.c
@@ -0,0 +1,228 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+// Área reservada para variveis de controle
+#define LOOP_COUNTER            *(unsigned short int*)(buffer)                                      
+#define COUNTER_CHAR            *(unsigned short int*)(buffer + (sizeof(unsigned short int) * 1))   
+#define TARGET_GET              *(unsigned short int*)(buffer + (sizeof(unsigned short int) * 2))
+#define WORD_COUNTER_TBN        *(unsigned short int*)(buffer + (sizeof(unsigned short int) * 3))
+#define WORD_LIST_LENGTH        *(unsigned short int*)(buffer + (sizeof(unsigned short int) * 4))
+#define OPERATION_INPUT         *(unsigned short int*)(buffer + (sizeof(unsigned short int) * 5))
+// Área reservada para input temporário (até 79 chars + '\0')
+#define WORD_INPUT_BASE         (sizeof(unsigned short int) * 6)   
+#define WORD_INPUT              ((char*)((char*)(buffer) + WORD_INPUT_BASE))
+// Área onde fica a lista de datas
+// No formato: [len_nome][nome][len_email][email][idade]
+#define WORD_LIST_BASE          (WORD_INPUT_BASE + (sizeof(char) * 80)) 
+#define WORD_LIST               ((char*)((char*)(buffer) + WORD_LIST_BASE))
+
+char *buffer = NULL;    
+
+void pushInfo() {
+    printf("Insert a name:\n");
+    scanf(" %78[^\n]", WORD_INPUT + 1);
+    WORD_INPUT[0] = (char) strlen(WORD_INPUT + 1);
+    buffer = realloc(buffer, WORD_LIST_BASE + (sizeof(char) * (strlen(WORD_LIST) + strlen(WORD_INPUT) + 1)));
+    strcat(WORD_LIST, WORD_INPUT);
+
+    printf("Insert an email:\n");
+    scanf(" %78[^\n]", WORD_INPUT + 1);
+    WORD_INPUT[0] = (char) strlen(WORD_INPUT + 1);
+    buffer = realloc(buffer, WORD_LIST_BASE + (sizeof(char) * (strlen(WORD_LIST) + strlen(WORD_INPUT) + 1)));
+    strcat(WORD_LIST, WORD_INPUT);
+
+    printf("Insert an age:\n");
+    scanf(" %78[^\n]", WORD_INPUT + 1);
+    WORD_INPUT[0] = (char) strlen(WORD_INPUT + 1);
+    buffer = realloc(buffer, WORD_LIST_BASE + (sizeof(char) * (strlen(WORD_LIST) + strlen(WORD_INPUT) + 1)));
+    strcat(WORD_LIST, WORD_INPUT);
+
+
+    WORD_LIST_LENGTH = WORD_LIST_LENGTH + 1; 
+    WORD_INPUT[0] = '\0';
+}
+
+void printPersonByTarget() {
+    if (TARGET_GET == 0 || TARGET_GET > WORD_LIST_LENGTH)
+        return;
+
+    WORD_COUNTER_TBN = 0;  
+    LOOP_COUNTER = 1;       
+
+    while (LOOP_COUNTER <= WORD_LIST_LENGTH) {
+        COUNTER_CHAR = (unsigned char) WORD_LIST[WORD_COUNTER_TBN];
+        WORD_COUNTER_TBN += 1;
+        if(LOOP_COUNTER == TARGET_GET)
+            printf("Name: %.*s\n", COUNTER_CHAR, WORD_LIST + WORD_COUNTER_TBN);
+        WORD_COUNTER_TBN += COUNTER_CHAR;
+
+        COUNTER_CHAR = (unsigned char) WORD_LIST[WORD_COUNTER_TBN];
+        WORD_COUNTER_TBN += 1;
+        if(LOOP_COUNTER == TARGET_GET)
+            printf("Email: %.*s\n", COUNTER_CHAR, WORD_LIST + WORD_COUNTER_TBN);
+        WORD_COUNTER_TBN += COUNTER_CHAR;
+
+        COUNTER_CHAR = (unsigned char) WORD_LIST[WORD_COUNTER_TBN];
+        WORD_COUNTER_TBN += 1;
+        if(LOOP_COUNTER == TARGET_GET)
+            printf("Age: %.*s\n\n", COUNTER_CHAR, WORD_LIST + WORD_COUNTER_TBN);
+        WORD_COUNTER_TBN += COUNTER_CHAR;
+
+        LOOP_COUNTER += 1;
+    }
+}
+
+// Procura pessoa pelo nome e guarda a posição em TARGET_GET (Se não achar -> SHRT_MAX)
+void searchPersonByName() {
+    printf("Search name:\n");
+    scanf(" %78[^\n]", WORD_INPUT + 1);
+    WORD_INPUT[0] = (char)strlen(WORD_INPUT + 1);
+
+    TARGET_GET = SHRT_MAX;  
+    
+    WORD_COUNTER_TBN = 0;
+    LOOP_COUNTER = 1;
+
+    while (LOOP_COUNTER <= WORD_LIST_LENGTH) {
+        COUNTER_CHAR = (unsigned char) WORD_LIST[WORD_COUNTER_TBN];
+        WORD_COUNTER_TBN += 1;
+        if (strncmp(WORD_LIST + WORD_COUNTER_TBN, WORD_INPUT + 1, COUNTER_CHAR) == 0) {
+            TARGET_GET = LOOP_COUNTER;
+            return;
+        }
+        WORD_COUNTER_TBN += COUNTER_CHAR;
+        COUNTER_CHAR = (unsigned char) WORD_LIST[WORD_COUNTER_TBN];
+        WORD_COUNTER_TBN += 1;
+        WORD_COUNTER_TBN += COUNTER_CHAR;
+        COUNTER_CHAR = (unsigned char) WORD_LIST[WORD_COUNTER_TBN];
+        WORD_COUNTER_TBN += 1;
+        WORD_COUNTER_TBN += COUNTER_CHAR;
+
+        LOOP_COUNTER += 1;
+    }
+}
+
+void removePersonByName() {
+    searchPersonByName();
+    if (TARGET_GET == 0 || TARGET_GET == SHRT_MAX){
+        printf("Person not found.\n");
+        return;
+    }
+
+    WORD_COUNTER_TBN = 0;  
+    LOOP_COUNTER = 1;       
+    COUNTER_CHAR = 0;
+    while (LOOP_COUNTER <= WORD_LIST_LENGTH) {
+        if(LOOP_COUNTER == TARGET_GET){
+            COUNTER_CHAR = (unsigned char) WORD_LIST[WORD_COUNTER_TBN];
+            COUNTER_CHAR += 1;
+            COUNTER_CHAR += (unsigned char) WORD_LIST[COUNTER_CHAR];
+            COUNTER_CHAR += 1;
+            COUNTER_CHAR += (unsigned char) WORD_LIST[COUNTER_CHAR];
+            COUNTER_CHAR += 1;
+
+            memmove(WORD_LIST + WORD_COUNTER_TBN, WORD_LIST + WORD_COUNTER_TBN + COUNTER_CHAR, strlen(WORD_LIST + WORD_COUNTER_TBN + COUNTER_CHAR) + 1);
+
+            WORD_LIST_LENGTH = WORD_LIST_LENGTH - 1;
+            buffer = realloc(buffer, WORD_LIST_BASE + (sizeof(char) * (strlen(WORD_LIST) + strlen(WORD_INPUT) + 1)));
+            return;
+        }
+
+        WORD_COUNTER_TBN = (unsigned char) WORD_LIST[WORD_COUNTER_TBN];
+        WORD_COUNTER_TBN += 1;
+        WORD_COUNTER_TBN += (unsigned char) WORD_LIST[WORD_COUNTER_TBN];
+        WORD_COUNTER_TBN += 1;
+        WORD_COUNTER_TBN += 1;
+
+        LOOP_COUNTER += 1;
+    }
+}
+
+void printAllInfo() {
+    WORD_COUNTER_TBN = 0;  
+    LOOP_COUNTER = 1;       
+
+    while (LOOP_COUNTER <= WORD_LIST_LENGTH) {
+        printf("Person %hu:\n", LOOP_COUNTER);
+        COUNTER_CHAR = (unsigned char) WORD_LIST[WORD_COUNTER_TBN];
+        WORD_COUNTER_TBN += 1;
+        printf("Name: %.*s\n", COUNTER_CHAR, WORD_LIST + WORD_COUNTER_TBN);
+        WORD_COUNTER_TBN += COUNTER_CHAR;
+
+        COUNTER_CHAR = (unsigned char) WORD_LIST[WORD_COUNTER_TBN];
+        WORD_COUNTER_TBN += 1;
+        printf("Email: %.*s\n", COUNTER_CHAR, WORD_LIST + WORD_COUNTER_TBN);
+        WORD_COUNTER_TBN += COUNTER_CHAR;
+
+        COUNTER_CHAR = (unsigned char) WORD_LIST[WORD_COUNTER_TBN];
+        WORD_COUNTER_TBN += 1;
+
+        printf("Age: %.*s\n\n", COUNTER_CHAR, WORD_LIST + WORD_COUNTER_TBN);
+        WORD_COUNTER_TBN += COUNTER_CHAR;
+
+        LOOP_COUNTER += 1;
+    }
+}
+
+int main() {
+    buffer = malloc(WORD_LIST_BASE + 1);
+    WORD_LIST_LENGTH = 0;
+    TARGET_GET = 0;
+    WORD_INPUT[0] = '\0'; 
+    WORD_LIST[0] = '\0';
+
+    do{
+        printf( "1- Add Person\n"
+                "2- Remove Person\n"
+                "3- Search Person\n"
+                "4- List All\n"
+                "5- Exit\n\n");
+        
+        scanf(" %hu", &OPERATION_INPUT);
+
+        switch (OPERATION_INPUT) {
+            case 1: { 
+                pushInfo();
+            } break;
+            case 2: { 
+                if (WORD_LIST_LENGTH == 0)
+                    printf("List is empty.\n");
+                else
+                  removePersonByName();
+            } break;
+
+            case 3: { 
+                if (WORD_LIST_LENGTH == 0) 
+                    printf("List is empty.\n");
+                else {
+                    searchPersonByName();
+                    if (TARGET_GET == 0 || TARGET_GET == SHRT_MAX)
+                        printf("Person not found.\n");
+                    else
+                        printPersonByTarget();
+                }
+            } break;
+
+            case 4: { 
+                printf("%s\n", WORD_LIST);
+                if (WORD_LIST_LENGTH == 0) 
+                    printf("List is empty.\n");
+                else 
+                    printAllInfo();  
+            } break;
+
+            case 5: {
+                printf("Exiting...\n");
+            } break;
+            
+            default: {
+                printf("Invalid Entry. Please, try again.\n\n");
+            } break;
+        }
+    } while (OPERATION_INPUT != 5);
+
+    free(buffer);
+    return 0;
+}
